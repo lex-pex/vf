75d42d2ca1eb874f1250fbe9202abe70bc82b5a0
Aleks <javaenginee@gmail.com>
2020-03-28 15:31:53 +0200

    fx

--- http/js/sl.js
+++ http/js/sl.js
@@ -1,4 +1,4 @@
-document.ondragstart = prevent;
-document.onselectstart = prevent;
+// document.ondragstart = prevent;
+// document.onselectstart = prevent;
 // document.oncontextmenu = prevent;
-function prevent() {return false;}
+// function prevent() {return false;}
